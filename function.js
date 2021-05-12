3500bef134aae82b59bcdc90373dcc7a3b36feea
w3slee <wbq6@tutanota.com>
2021-05-12 20:11:22 +0300

    cusor change button

--- function.js
+++ function.js
@@ -1,33 +1,3 @@
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-
 // Code By Webdevtrick ( https://webdevtrick.com )
 function showTime() {
     var date = new Date();
@@ -56,5 +26,4 @@ function showTime() {
     setTimeout(showTime, 1000);
 
 }
-
 showTime();
---
+++ script.js
@@ -0,0 +1,16 @@
+// cursor switcher
+
+let html = document.html;
+
+let funBtn = document.getElementById('nofun');
+let nofunBtn = document.getElementById('fun');
+
+funBtn.addEventListener('click', () => {
+    body.classList.remove('fun');
+	body.classList.add('nofun');
+});
+
+nofunBtn.addEventListener('click', () => {
+    body.classList.remove('nofun');
+    body.classList.add('fun');
+});
